61438c11f0e2fafdac9f3b1e5e13f7c50551b1ec
SeedThePlant <rynekleszczynski@gmail.com>
2016-12-22 22:32:28 +0100

    ground sie poprawnie spawnuje, kamienie sie spawnuja tez poprawnie ale czasem wisza w powietrzu

--- Assets/Scripts/SpawnGround.cs
+++ Assets/Scripts/SpawnGround.cs
@@ -7,32 +7,54 @@ public class SpawnGround : MonoBehaviour {
     public GameObject newGround;
     private Vector2 posA;
     private Vector2 posB;
+    private Vector2 posK; // pozycja kulki
     private Vector2 pos;
-    private float x, y;
+    private float x, y, a, b, bp; // a b i bp to zmienne dla funkcji rownoleglych
+    private float py; //odleglosc do powierzchni sniegu od prostej utworzonej ze srodkow 
     private float time;
-	// Use this for initialization
+
 	void Start () {
         posA = ground.transform.position;
         posB = ground2.transform.position;
         x = posB.x - posA.x;
-        y = posB.y - posB.y;
+        y = posB.y - posA.y;
         Debug.Log(x); Debug.Log(y);
         pos = posB;
+        InitialzeFunc(GameObject.Find("sniezka").gameObject.transform.position);
     }
-	
-	// Update is called once per frame
 	void Update () {
         time += Time.deltaTime;
-        if(time > 3.0f)
+        if(time > 3.0f) // zrobic nie czas a pozycja sniezki
         {
             Spawn();
             time = 0.0f;
         }
 	}
+    // funkcja wylicza pozycje i spawnuje ground'a
     private void Spawn()
     {
         Vector2 temp = new Vector2(x, y);
         pos += temp;
         Instantiate(newGround, pos, Quaternion.identity);
+    }
+    // funkcja przyjmuje aktualna pozycje kulki;
+    public Vector2 ReturnPos(Vector2 pT)
+    {
+        posK = pT;
+        posK.x += Random.Range(15.0f, 30.0f);
+        posK.y = a * posK.x + bp; //y=ax+b
+        return posK;
+    }
+    public void InitialzeFunc(Vector2 pT)
+    {
+        posK = pT;
+        //wyznaczenie prostej prostopadlej
+        a = (posB.y - posA.y) / (posB.x - posA.x);
+        b = posA.y - posA.x * a;
+        float c = (-1) / a;
+        float d = posK.y - posK.x * c;
+        Vector2 PP = new Vector2((b - d) / (a - c), (b - d) / (a - c) * a - b); // punkt przeciecia dwoch funkcji
+        float length = Mathf.Sqrt(Mathf.Pow(PP.x + posK.x, 2) + Mathf.Pow(PP.y + posK.y, 2)); //odleglosc
+        bp = b + length; // bprim
     }
 }
--- Assets/Scripts/SpawnRocks.cs
+++ Assets/Scripts/SpawnRocks.cs
@@ -2,14 +2,23 @@
 using System.Collections;
 
 public class SpawnRocks : MonoBehaviour {
-
+    public GameObject rock;
+    private Vector2 pos;
+    private float time;
+    private float delta;
 	// Use this for initialization
 	void Start () {
-	
+        delta = Random.Range(1.0f, 2.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        time += Time.deltaTime;
+	    if (time > delta)
+        {
+            pos = Camera.main.GetComponent<SpawnGround>().ReturnPos(GameObject.Find("sniezka").gameObject.transform.position);
+            Instantiate(rock, pos, Quaternion.identity);
+            time = 0.0f;
+        }
 	}
 }
