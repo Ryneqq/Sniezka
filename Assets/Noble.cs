f45f8f901ec804f9f17d38361348e0b0de5f8ccd
SeedThePlant <rynekleszczynski@gmail.com>
2017-02-24 17:40:39 +0100

    Added Noble Games logo

--- Assets/Noble.cs
+++ Assets/Noble.cs
@@ -4,14 +4,16 @@ using UnityEngine.UI;
 
 public class Noble : MonoBehaviour {
     private float time;
+    public float executionTime;
 	void Awake () {
         time = 0.0f;
+        //executionTime = 1.0f;
         if(!Variables.start)
             gameObject.GetComponent<Image>().enabled = false;
     }
 	void Update () {
         time += Time.deltaTime;
-        if (time >= 1.0f && Variables.start)
+        if (time >= executionTime && Variables.start)
         {
             gameObject.GetComponent<Image>().enabled = false;
             Variables.start = false;
